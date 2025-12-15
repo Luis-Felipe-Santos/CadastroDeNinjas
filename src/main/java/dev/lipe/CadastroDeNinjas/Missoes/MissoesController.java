8dc21c00dbfff2a8d170f79ca7c3da418b773975
Luis-Felipe-Santos <luysfelyp07@gmail.com>
2025-12-15 13:01:47 -0300

    Modelagem do banco de dados, NinjaModel e MissoesModel concluidos, annotations feitas para relacionamento de tabelas

---
+++ src/main/java/dev/lipe/CadastroDeNinjas/Missoes/MissoesController.java
@@ -0,0 +1,4 @@
+package dev.lipe.CadastroDeNinjas.Missoes;
+
+public class MissoesController {
+}
--- src/main/java/dev/lipe/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/lipe/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,7 @@
-package dev.lipe.CadastroDeNinjas;
+package dev.lipe.CadastroDeNinjas.Ninjas;
 
 
+import dev.lipe.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
 
 // Entity - transforma uma classe em uma entidade do banco de dados
@@ -12,9 +13,17 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
     private int idade;
+
+    //@ManyToOne Um ninja tem uma unica missao
+    @ManyToOne
+    @JoinColumn(name = "missoes_id") // Foreign Key ou chave estrangeira
+    private MissoesModel missoes;
 
     public NinjaModel() {
     }
